c268c0d7ed4c2736c4fc1e4de8fb7f7d3dca0da0
chungthaidung <55013649+chungthaidung@users.noreply.github.com>
2019-10-22 23:07:01 +0700

    Upload source code form Hoadon ,Nhanvien, Trangchu
    Upload

--- qlktxserver/qlktxserver/FormTrangChu.cs
+++ qlktxserver/qlktxserver/FormTrangChu.cs
@@ -1,25 +1,231 @@
-﻿using System;
+﻿using qlktxserver;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace qlktxserver
+namespace QLKTX
 {
     public partial class FormTrangChu : Form
     {
         public FormTrangChu()
         {
             InitializeComponent();
+            
+
+
+
         }
 
-        private void FormTrangChu_Load(object sender, EventArgs e)
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            Process myProcess = new Process();
+            try
+            {
+                // true is the default, but it is important not to set it to false
+                myProcess.StartInfo.UseShellExecute = true;
+                myProcess.StartInfo.FileName = "http://ktx.vnuhcm.edu.vn/";
+                myProcess.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            //FormDangKy formDangKy = new FormDangKy();
+            //formDangKy.Show();
+            frmDoiMatKhau FormDoiMatKhau = new frmDoiMatKhau();
+            FormDoiMatKhau.Show();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //txtMssv.text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+        
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+        private void toolStripMenuItem9_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void hóaĐơnToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void sinhViênToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void hệThốngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                hoadon1.Visible = false;
+                nhanvien1.Visible = false;
+                sinhvien1.Visible = false;
+                danhsachphongo1.Visible = true;
+            tkb1.Visible = false;
+
+        }
+
+        private void nhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hoadon1.Visible = false;
+            nhanvien1.Visible = true;
+            sinhvien1.Visible = false;
+            danhsachphongo1.Visible = false;
+            tkb1.Visible = false;
+        }
+
+        private void sinhViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hoadon1.Visible = false;
+            nhanvien1.Visible = false;
+            sinhvien1.Visible = true;
+            danhsachphongo1.Visible = false;
+            tkb1.Visible = false;
+        }
+
+        private void hóaĐơnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hoadon1.Visible = true;
+            nhanvien1.Visible = false;
+            sinhvien1.Visible = false;
+            danhsachphongo1.Visible = false;
+            tkb1.Visible = false;
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+      public  void Thoat()
+        {
+            nhanvien1.Visible = false;
+            sinhvien1.Visible = false;
+            danhsachphongo1.Visible = false;
+            hoadon1.Visible = true;
+            tkb1.Visible = false;
+           
+        }
+        private void FormTrangChu_Load(object sender, EventArgs e)
+        {
+            
+            Thoat();
+            Nhandulieu minh = new Nhandulieu();
+            minh.Connect();
+            Form1 minh2 = new Form1();
+            minh2.Connect();
+            Anh minh3 = new Anh();
+            minh3.Connect();
+            chat minh5 = new chat();
+            minh5.Connect();
+           
+        }
+        public byte[] ImageToByteArray1(Image image, string extension)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                switch (extension)
+                {
+                    case ".jpeg":
+                    case ".jpg":
+                        image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case ".png":
+                        image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    case ".gif":
+                        image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                }
+                return memoryStream.ToArray();
+            }
+        }
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chat1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void danhsachphongo1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            var dlgResult = MessageBox.Show("Bạn Thực Sự Muốn Thoát Chương Trình ?", "?", MessageBoxButtons.YesNo);
+            if (dlgResult == System.Windows.Forms.DialogResult.Yes)
+            {
+                
+                this.Close(); // close the connection setup form & back to login form
+                frmDangNhap f = new frmDangNhap();
+                f.Show();
+               // Application.Exit(); //exit
+            }
+            
+        }
+
+        private void trợGiupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nhanvien1.Visible = false;
+            sinhvien1.Visible = false;
+            danhsachphongo1.Visible = false;
+            hoadon1.Visible = false;
+            tkb1.Visible = true;
         }
     }
 }
--- qlktxserver/qlktxserver/Hoadon.cs
+++ qlktxserver/qlktxserver/Hoadon.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace qlktxserver
 {
@@ -15,6 +16,84 @@ namespace qlktxserver
         public Hoadon()
         {
             InitializeComponent();
+        }
+        SqlConnection connect;
+        DataTable data = new DataTable();
+        public void Hienthi()
+        {
+            try
+            {
+                string query = "SELECT ThanhTien,ChiSoNuoc,TongSP FROM dbo.HOADON WHERE MaPHG='" + textBox1.Text + "' AND Thang=" + comboBox4.Text;
+                SqlCommand cmd = new SqlCommand(query, connect);
+                //  cmd.Parameters.AddWithValue("MaNV", textBox1.Text);
+                //  cmd.Parameters.AddWithValue("HoTenNV", textBox2.Text);
+                // cmd.ExecuteNonQuery();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+
+
+                adapter.Fill(data);
+
+
+
+                //dataGridView1.DataSource = data;
+                connect.Close();
+            }
+            catch(Exception ex)
+            { 
+
+            connect.Close();
+            }
+        }
+            private void Hoadon_Load(object sender, EventArgs e)
+        {
+           // string connection = "Data Source=.\\SQLEXPRESS;Initial Catalog=QL;Integrated Security=True";
+            connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\hoc\test project\Test-master\Database\ACCOUNT.mdf;Integrated Security=True;Connect Timeout=30");
+            connect.Open();
+
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "" || comboBox4.Text == "") MessageBox.Show("Mã Phòng, Tháng Không Thể Trống", "?", MessageBoxButtons.OK);
+                else
+                {
+                    Hienthi();
+                    DataRow dr = data.Rows[0];
+                    string num = dr["ThanhTien"].ToString();
+                    textBox2.Text = dr["ChiSoNuoc"].ToString();
+                    textBox5.Text = dr["TongSP"].ToString();
+                    textBox6.Text = num;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
         }
     }
 }
