2f590f13cf30f4d2c5b8476a7c2fbac19ea07d76
tranphuongduy20 <55152196+tranphuongduy20@users.noreply.github.com>
2019-11-13 16:19:00 +0700

    Update Danhsachphongo.cs

--- qlktxserver/qlktxserver/Danhsachphongo.cs
+++ qlktxserver/qlktxserver/Danhsachphongo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using qlktxserver;
+using QLKTX;
 
 namespace qlktxserver
 {
@@ -15,6 +17,36 @@ namespace qlktxserver
         public Danhsachphongo()
         {
             InitializeComponent();
+        }
+        public void Hienthi()
+        {
+            string query = "SELECT * FROM dbo.PHONG ";
+            DataProvider load = new DataProvider();
+            dataGridView1.DataSource = load.ExecuteQuery(query);
+        }
+        private void Danhsachphongo_Load(object sender, EventArgs e)
+        {
+            Hienthi();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Refresh();
+            string query = "SELECT *FROM dbo.PHONG WHERE MAPHG '" +textBox1+ "'";
+            DataProvider load = new DataProvider();
+            dataGridView1.DataSource = load.ExecuteQuery(query);
+        }
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
