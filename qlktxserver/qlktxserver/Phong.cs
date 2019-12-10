9994d409ef34e8e56892f910034ea14591e0884b
quanghoang0403 <55055574+quanghoang0403@users.noreply.github.com>
2019-12-10 12:00:15 +0700

    Add files via upload

---
+++ qlktxserver/qlktxserver/Phong.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qlktxserver
+{
+    public class Phong
+    {
+        public Phong(int iD, string status)
+        {
+            this.ID = iD;
+            this.Status = status;
+        }
+
+        public Phong(DataRow row)
+        {
+            this.ID = (int)row["MAPHG"];
+            this.Status = row["TINHTRANG"].ToString();
+        }
+        private int iD;
+        public int ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+
+        private string status;
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                status = value;
+            }
+        }
+    }
+}
